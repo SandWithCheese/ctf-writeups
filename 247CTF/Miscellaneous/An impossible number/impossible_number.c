bbbc5df1353671ff9413dc44444f51686fe6f34b
Ahmad Naufal Ramadan <83053460+SandWithCheese@users.noreply.github.com>
2024-03-13 11:03:09 -0400

    feat: added 247CTF misc chall

---
+++ 247CTF/Miscellaneous/An impossible number/impossible_number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int impossible_number;
+    FILE *flag;
+    char c;
+    if (scanf("%d", &impossible_number)) {
+        if (impossible_number > 0 && impossible_number > (impossible_number + 1)) {
+            flag = fopen("flag.txt","r");
+            while((c = getc(flag)) != EOF) {
+                printf("%c",c);
+            }
+        }
+    }
+    return 0;
+}
